a6fef3fe486cca575919b25218eabf26af78bd8a
Ruslan <hellperock@gmail.com>
2024-01-31 14:28:30 +0500

    Initial Commit

---
+++ SparMock/SparMockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparMockApp.swift
+//  SparMock
+//
+//  Created by Ruslan Ismailov on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SparMockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
